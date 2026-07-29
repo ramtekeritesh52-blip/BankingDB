03774ade722c73f3d24a4402bcf2cea70c913272
ramtekeritesh52-blip <ramtekeritesh52@gmail.com>
2026-07-29 09:17:28 +0530

    created new tables

--- Banking.sql
+++ Banking.sql
@@ -1,6 +1,6 @@
 -- show databases;
 -- create database BankingDB;
-USE bankingdb;
+-- USE bankingdb;
 CREATE TABLE Customers(
 	CustomerID INT PRIMARY KEY,
     FirstName VARCHAR(50),
@@ -10,8 +10,47 @@ CREATE TABLE Customers(
     AccountCreationDate date
 );
 
-describe customers;
-select * from Customers;
+create table Accounts(
+	AccountID int,
+    AccountType varchar(20),
+    Balance decimal(10,2)
+);
+-- desc Accounts;
+
+create table Transactions(
+	TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+-- desc transactions;
+
+create table Branches(
+	BranchId int,
+    BranchName varchar(100),
+    BranchAddress varchar(200),
+    BranchPhone varchar(15)
+);
+-- desc branches;
+
+create table AccountBranches(
+	AssignmentDate DATE
+);
+
+create table Loans(
+	LoanId int,
+    LoanAmount decimal(10,2),
+    InterestRate decimal(5,2),
+    StartDate date,
+    EndDate date
+);
+
+
+
+
+
+
+-- select * from Customers;
 
 
 
